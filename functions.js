cc8edad02a31375efb5f16f0c99c4e7bdc6cd54f
Daniel Porumb <daniel.porumb@reyno.co.uk>
2018-12-21 21:39:52 +0200

    save poijt

---
+++ functions.js
@@ -0,0 +1,14 @@
+function exerciseJavascript() {
+    var initialNumber = 0;
+    var myString = "";
+    var myArray = [];
+
+    do {
+        initialNumber;
+        myArray.push(initialNumber);
+    }
+    while (initialNumber == 0);
+
+    return myString + myArray;
+}
+console.log(exerciseJavascript());
---
+++ myJavascript.js
@@ -0,0 +1,18 @@
+
+var person = {
+    nume: 'Sergionx',
+    prenume: 'Mihail',
+    varsta: 23,
+    sex: 'M',
+    pasiuni: [
+        'hokey', 'Fotbal', 'citit', 'scris'
+    ],
+    address: {
+        strada: 21 + 'dec',
+        number: 17,
+        bloc: 'c17'
+    }
+};
+
+alert(`Am o persona cu proprietatile ${person.pasiuni[0]}, ${person.pasiuni[1]}`);
+console.log(person);
